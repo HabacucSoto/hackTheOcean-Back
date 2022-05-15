48e72b951bf8b695de0e77eb689c173d2a5c53c3
habacuc <josehabacuc182020@gmail.com>
2022-05-15 18:21:36 -0500

    code for method allReports in class ReportService and test for it

---
+++ test/lib/services/ReportService.test.js
@@ -0,0 +1,15 @@
+const Reader = require( "../../../lib/utils/Reader.js" );
+const reports = Reader.readJsonFile( "reports.json" );
+const ReportService = require( "../../../lib/services/ReportService.js" );
+
+describe( "Unit test for class ReportService", () => {
+
+    test( "1) Test for method allReports", () => {
+
+        const result = ReportService.allReports( reports );
+        
+        expect( result ).not.toBeUndefined();
+
+    } )
+
+} )
